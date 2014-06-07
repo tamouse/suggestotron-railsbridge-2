627aadbf85a40878918182f9a7716350a4b99609
Tamara Temple <tamouse@gmail.com>
2014-06-07 13:56:14 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuggestotronRailsbridge2::Application.config.secret_key_base = '34c9f317e692819d675360c9fc1b3c994ed77bb19d53212743d4ddc252e644d8fd50c07f418973608ccc2361eb9d70b7871f728f6b43f131c22c1c6a726de33e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SuggestotronRailsbridge2::Application.config.session_store :cookie_store, key: '_suggestotron-railsbridge-2_session'
